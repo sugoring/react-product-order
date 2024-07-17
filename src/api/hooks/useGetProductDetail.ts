12c9e4715c6a8672a925ee3ba2e1096abbd4c75e
yuni <hys990730@gmail.com>
2024-07-17 17:59:51 +0900

    feat: 상품 관련 API 훅 분리 및 개선
    - enabled 옵션 추가를 통해 productId 값 유효성 검사 후 쿼리 실행하여 불필요한 API 호출 방지

--- src/api/hooks/useGetProductDetail.ts
+++ src/api/hooks/useGetProductDetail.ts
@@ -5,18 +5,15 @@ import type { ProductDetailData } from '@/types';
 
 import { fetchInstance } from '../instance';
 
-const getProductDetailPath = (productId: string) => `/v1/products/${productId}/detail`;
-
-export const getProductDetail = async (productId: string) => {
-  const response = await fetchInstance.get<ProductDetailData>(getProductDetailPath(productId));
-  return response.data; // 필요에 따라 데이터 변환 로직 추가
-};
-
 export const useGetProductDetail = (productId: string) => {
+  const queryKey = ['productDetail', productId];
+
   return useQuery<ProductDetailData, AxiosError>({
-    queryKey: ['productDetail', productId],
-    queryFn: () => getProductDetail(productId),
-    // 오류 처리 로직 추가 (필요에 따라)
-    // enabled: !!productId, // productId가 있을 때만 쿼리 실행
+    queryKey,
+    queryFn: async () => {
+      const { data } = await fetchInstance.get<ProductDetailData>(`/v1/products/${productId}/detail`);
+      return data;
+    },
+    enabled: !!productId, 
   });
 };
--- src/api/hooks/useGetProductOptions.ts
+++ src/api/hooks/useGetProductOptions.ts
@@ -5,22 +5,19 @@ import type { ProductOption, ProductOptionData } from '@/types';
 
 import { fetchInstance } from '../instance';
 
-const getProductOptionsPath = (productId: string) => `/v1/products/${productId}/options`;
-
-export const getProductOptions = async (productId: string): Promise<ProductOption[]> => {
-  const { data } = await fetchInstance.get<ProductOptionData>(getProductOptionsPath(productId));
-  return data.options.map((option) => ({
-    id: option.id,
-    name: option.value,
-    price: option.price,
-  }));
-};
-
 export const useGetProductOptions = (productId: string) => {
+  const queryKey = ['productOptions', productId];
+
   return useQuery<ProductOption[], AxiosError>({
-    queryKey: ['productOptions', productId],
-    queryFn: () => getProductOptions(productId),
-    // 오류 처리 로직 추가 (필요에 따라)
-    // enabled: !!productId, // productId가 있을 때만 쿼리 실행
+    queryKey,
+    queryFn: async () => {
+      const { data } = await fetchInstance.get<ProductOptionData>(`/v1/products/${productId}/options`);
+      return data.options.map((option) => ({
+        id: option.id,
+        name: option.value,
+        price: option.price,
+      }));
+    },
+    enabled: !!productId, // productId가 있을 때만 쿼리 활성화
   });
 };
